be57477770a747fdb06f3a2ae2221a6943e0c817
B1ank <b1ank07@mail.ru>
2024-08-06 23:10:45 +0300

    добавлена функция суммы

--- main.go
+++ main.go
@@ -3,15 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	for i := 1; i <= 100; i++ {
-		if i%15 == 0 {
-			fmt.Println("FizzBuzz")
-		} else if i%3 == 0 {
-			fmt.Println("Fizz")
-		} else if i%5 == 0 {
-			fmt.Println("Buzz")
-		} else {
-			fmt.Println(i)
-		}
-	}
+	fmt.Println(sum(2, 2))
+}
+
+func sum(a int, b int) int {
+	return a + b
 }
